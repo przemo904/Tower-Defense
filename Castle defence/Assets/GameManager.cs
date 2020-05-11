147e060d1a30e9efd28b29a4e72fde161be1b4d1
przemo904 <przemosoft904@gmail.com>
2020-05-11 18:54:11 +0200

    przemek_scenes

---
+++ Castle defence/Assets/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    [SerializeField] public GameObject wall;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.visible = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // wall selected from UI
+        // create wall model if possible
+        if (Input.GetMouseButton(0))
+        {
+            
+        }
+        if (Input.GetMouseButtonUp(0)) {
+            Instantiate(wall, new Vector3(0, 1, 0), Quaternion.Euler(-90, 0, 0));
+        }
+    }
+
+}
